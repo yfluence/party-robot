170781409d18520e4033acfddd1aa8a01cd0bae9
Yeemon T <155947272+yfluence@users.noreply.github.com>
2024-11-06 21:07:55 +0000

    Finish happybirthday and assigntable functions

--- party_robot.go
+++ party_robot.go
@@ -9,10 +9,10 @@ func Welcome(name string) string {
 
 // HappyBirthday wishes happy birthday to the birthday person and exclaims their age.
 func HappyBirthday(name string, age int) string {
-	return fmt.Sprintf("Complete this function")
+	return fmt.Sprintf("Happy birthday %s! You are now %d years old!", name, age)
 }
 
 // AssignTable assigns a table to each guest.
 func AssignTable(name string, table int, neighbor, direction string, distance float64) string {
-	return fmt.Sprintf("Complete this function")
+	return fmt.Sprintf("Welcome to my party, %s!\nYou have been assigned to table %03d. Your table is %s, exactly %.1f meters from here.\nYou will be sitting next to %s.", name, table, direction, distance, neighbor)
 }
